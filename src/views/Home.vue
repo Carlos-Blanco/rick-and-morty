89b4fb88d97de61ad72a77e4e077b62605286d27
Carlos Blanco <carlosblanco@me.com>
2020-11-19 19:50:13 +0100

    Hidding component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <h1>Characters</h1>
-    <Card msg="Welcome to Your Vue.js App"/>
+    <!-- <Card msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Card from '@/components/Card.vue'
+/* import Card from '@/components/Card.vue'
 
 export default {
   name: 'Home',
   components: {
     Card
   }
-}
+} */
 </script>
